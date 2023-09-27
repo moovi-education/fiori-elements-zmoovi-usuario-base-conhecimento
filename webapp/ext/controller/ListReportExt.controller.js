89756dd0b20ab016792fbd825dec9cab892fdae2
Luiz <leduardo9753@gmail.com>
2023-09-28 02:26:31 +1300

    first commit Moovi :rocket:

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([],
+function (){
+    "use strict";
+    return {
+        onInitSmartFilterBarExtension: function(oEvent){
+            var sUser = sap.ushell.Container.getService("UserInfo").getId(),
+            oDefaultFilter = {
+                "Userid": sUser
+            };
+            this.byId("listReportFilter").setFilterData(oDefaultFilter);
+            this.byId("listReportFilter").determineFilterItemByName("Userid").setVisibleInFilterBar(false);
+        }
+    };
+});
